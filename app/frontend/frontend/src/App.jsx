37789e0571ac1119ad69ab037bc1cc43ca12798d
Yanis BESTAOUI <yanisbestaoui@mbp-de-yanis.home>
2025-01-06 19:30:56 +0100

    Setup React frontend connected to backend

---
+++ app/frontend/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/') // Adresse de votre API backend
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Frontend Connected to Backend</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
